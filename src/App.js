a7c6142c80e74772bab59db37883e61ff242d5bf
Chandrashekar <Chandrashekar@PF3YLV0X.3SCSOLUTION.LAN>
2024-11-05 10:26:45 +0530

    added jenkins file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          changed
+          running via jenkins
         </a>
       </header>
     </div>
